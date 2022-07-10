4617b357838ec3c4bbbac0271b47178ccbbf1f49
Nick Whitt <nick.whitt@protonmail.com>
2022-07-09 20:12:06 -0400

    factories

--- __tests__/batter.js
+++ __tests__/batter.js
@@ -1,11 +1,12 @@
 import { Batter } from '../models';
+import { BatterFactory } from '../factories/batter';
 
 beforeEach(async () => {
   await Batter.sync({ force: true });
 });
 
 test('foo', async () => {
-  const batter = await Batter.create({ name: 'bar' });
+  const batter = await BatterFactory.build();
   console.debug(batter.toJSON());
 
   expect(batter).toBeInstanceOf(Batter);
